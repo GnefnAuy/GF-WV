e35bf34501273fbd427b1436b5b50fd548c80393
yuan feng <126933847+GnefnAuy@users.noreply.github.com>
2023-05-29 15:52:22 +0800

    theoretical result of WM

--- WM.py
+++ WM.py
@@ -70,7 +70,7 @@ def Sampling(RDN, N, pointer_final, up, down, up_x, down_x, up_y, down_y):
     return PR_x, PR_y, PR_z
 
 
-def MDresult(g,xi,yi,zi, MX, MY, MZ):
+def MeasuredResult(g,xi,yi,zi, MX, MY, MZ):
     xf, yf, zf = MX, MY, MZ
     IM=(yf-yi)/(2*g*(1-yi*yi))
     RM=(zf-zi+2*g*IM*zi*yi)/(-2*g*xi)
