fad7d18be09d5c17507fba2bd9c6aa35c0a53b47
Yuan Feng <126933847+GnefnAuy@users.noreply.github.com>
2024-04-11 14:29:33 +0100

    Update Fig6_subplot.py

--- Relevant Figures/Fig6_subplot.py
+++ Relevant Figures/Fig6_subplot.py
@@ -1,1 +1,62 @@
-import matplotlib as mplfrom matplotlib import cmimport matplotlib.pyplot as pltfrom qutip import *import numpy as npimport seaborn as sns  from matplotlib import colors  from matplotlib import rc#plt.rcdefaults()mpl.rcParams['font.family']= 'sans-serif'mpl.rcParams['font.serif'] = 'Computer Modern Roman'mpl.rcParams['font.sans-serif']= 'Computer Modern Sans Serif'mpl.rcParams['mathtext.fontset']='stix'mpl.rcParams['mathtext.rm'] = 'sans'mpl.rcParams['mathtext.it'] = 'sans:italic'mpl.rcParams['mathtext.default']='it'mpl.rcParams['mathtext.bf']= 'sans:bold'sns.set(style="darkgrid")sns.set_context("poster", font_scale = 0.25 , rc={"grid.linewidth": 1})clis = plt.cm.rainbow_r(np.linspace(0,1,10))#Plt realfig,axs=plt.subplots( 2, 4, figsize=(6.772, 2), sharey=False, sharex=False)#,layout='constrained')#,gridspec_kw={'height_ratios': [1, 1, 1, 0.12]})plt.subplots_adjust(hspace=0.55,wspace=0.5)
+import matplotlib as mpl
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from qutip import *
+import numpy as np
+import seaborn as sns  
+
+from matplotlib import colors  
+from matplotlib import rc
+#plt.rcdefaults()
+mpl.rcParams['font.family']= 'sans-serif'
+mpl.rcParams['font.serif'] = 'Computer Modern Roman'
+mpl.rcParams['font.sans-serif']= 'Computer Modern Sans Serif'
+mpl.rcParams['mathtext.fontset']='stix'
+mpl.rcParams['mathtext.rm'] = 'sans'
+mpl.rcParams['mathtext.it'] = 'sans:italic'
+mpl.rcParams['mathtext.default']='it'
+mpl.rcParams['mathtext.bf']= 'sans:bold'
+
+
+sns.set(style="darkgrid")
+sns.set_context("poster", font_scale = 0.2 , rc={"grid.linewidth": 1})
+
+
+N_index=np.load("N_index.npy")/1000
+DN = len(N_index)
+
+RL_AVE = np.load("RL_AVE.npy")
+RL_DEV = np.load("RL_DEV.npy") /1000
+Delta_ = np.load("Delta_.npy")
+
+
+
+clis = plt.cm.rainbow_r(np.linspace(0,1,10))
+
+#Plt real
+fig,axs=plt.subplots( 2, 4, figsize=(6.772, 1.5), sharey=False, sharex=False)#,layout='constrained')#,gridspec_kw={'height_ratios': [1, 1, 1, 0.12]})
+plt.subplots_adjust(hspace=0.7,wspace=0.5)
+
+axs[1,0].scatter(N_index, RL_DEV[0,0,:], s=5, linewidth=0.1, color = clis[0,:],marker='*' )
+axs[1,0].scatter(N_index, RL_DEV[0,1,:], s=5, linewidth=0.3, color = clis[1,:],  marker='x' )
+axs[1,0].scatter(N_index, RL_DEV[0,2,:], s=5, linewidth=0.1, color = clis[2,:], marker='^' )
+axs[1,0].scatter(N_index, RL_DEV[0,3,:], s=5, linewidth=0.1, color = clis[3,:], marker='s' )
+axs[1,0].scatter(N_index, RL_DEV[0,4,:], s=5, linewidth=0.1, color=clis[4,:],  marker='h' ) 
+axs[1,0].scatter(N_index, RL_DEV[0,5,:], s=5, linewidth=0.1, color=clis[5,:],marker='o' )
+axs[1,0].scatter(N_index, RL_DEV[0,6,:], s=5, linewidth=0.1, color=clis[6,:],  marker='d' )
+axs[1,0].scatter(N_index, RL_DEV[0,7,:], s=5, linewidth=0.1, color=clis[7,:], marker='<' )
+axs[1,0].scatter(N_index, RL_DEV[0,8,:], s=5, linewidth=0.1, color=clis[8,:], marker='p' )
+axs[1,0].scatter(N_index, RL_DEV[0,9,:], s=5, linewidth=0.1, color=clis[9,:],  marker='1' ) 
+axs[1,0].set_title('(e)$~Z_{\mathrm{RV}}$')
+axs[1,0].set(xlabel='$10^3~N$', ylabel='$10^3~1/\delta^2$')
+
+
+for i in [0,1]:
+    for j in [0,1,2,3]:
+        axs[i,j].tick_params(pad=-8)
+cmap = mpl.cm.rainbow
+norm = mpl.colors.Normalize(vmin=0.1, vmax=1)
+cbar=fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=axs[:], pad=0.03,orientation='vertical',ticks=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+cbar.ax.tick_params(width=0.5,length=3)
+plt.savefig('Fig6.png',dpi=400,bbox_inches ='tight')
+plt.show()
